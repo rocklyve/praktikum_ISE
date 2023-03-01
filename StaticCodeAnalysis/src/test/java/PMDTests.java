00cd250743a503eba21fc46efc72c30a1f2e6829
David Laubenstein <david.laubenstein@posteo.de>
2023-03-01 11:44:24 +0100

    Remove unimplemented rules, but referenced as comment

--- StaticCodeAnalysis/src/test/java/PMDTests.java
+++ StaticCodeAnalysis/src/test/java/PMDTests.java
@@ -90,57 +90,24 @@ public class PMDTests {
         return Stream.of(
                 // This is a custom rule, which detects, if any code in the codebase has system dependent line breaks
                 Arguments.of("Test SystemDependentLineBreak", List.of("SystemDependentLineBreakNotAllowed")),
-//                Arguments.of("Test RawType", List.of()),
                 Arguments.of("Test ConcreteClassInsteadOfInterface", List.of("LooseCoupling")),
                 // this is a custom rule, which detects assert statements in public functions,
                 // but only if they are the first statement
                 Arguments.of("Test AssertInsteadOfIfLoop", List.of("AssertStatementFirstInPublicFunction")),
-//                Arguments.of("Test ObjectInsteadOfConcreteClass", List.of()),
                 // this is a custom rule, which detects, if there are public enums inside of classes or interfaces
                 Arguments.of("Test PublicEnumInsideClassAndNotInSeparateFile", List.of("PublicEnumInsideClassOrInterface")),
-//                Arguments.of("Test VisibilityAsLowAsPossible", List.of()),
-//                Arguments.of("Test Code Duplication", List.of()),
-//                Arguments.of("Test CodeDuplicationRepetitionsFixableByInheritance", List.of()),
-//                Arguments.of("Test InheritanceInsteadOfEnums", List.of()),
-//                Arguments.of("Test OperationsInsteadOfDomain", List.of()),
                 Arguments.of("Test HardcodedLogic", List.of("AvoidLiteralsInIfCondition")),
-//                Arguments.of("Test StringReferences", List.of()),
                 Arguments.of("Test ExceptionsForControlFlow", List.of("EmptyCatchBlock")),
                 Arguments.of("Test TryCatchBlock", List.of("TooLongTryBlockStatement")),
-//                Arguments.of("Test UnspecifiedErrorMessage", List.of(RULE_PREFIX + )),
                 Arguments.of("Test WrongLoopType", List.of("ForLoopCanBeForeach", "ForLoopShouldBeWhileLoop")),
-//                Arguments.of("Test UnnecessaryComplexity", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test ClumsySolution", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test ParsingIntegerValues", List.of(RULE_PREFIX + )),
                 Arguments.of("Test UtilityClass", List.of("UseUtilityClass")),
-//                Arguments.of("Test UnsafeCast", List.of(RULE_PREFIX + )),
                 Arguments.of("Test EmptyConstructor", List.of("UncommentedEmptyConstructor", "UnnecessaryConstructor")),
-//                Arguments.of("Test MeaninglessConstant", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test Scanner", List.of(RULE_PREFIX + )),
                 Arguments.of("Test UnusedElement",
                         List.of("UnusedPrivateField", "UnusedPrivateMethod", "UnusedLocalVariable", "UnusedFormalParameter")
                 ),
-//                Arguments.of("Test MissingThrowsInMethodSignature", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test PublicEnumInClass", List.of(RULE_PREFIX + )),
                 // this is a custom pmd rule, which detects, if classes only keeps constants, but no other attributes and functions
                 Arguments.of("Test ClassOfConstants", List.of("ClassOfConstants")),
-//                Arguments.of("Test ParsingIntegerValues", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test TrivialJavaDoc", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test BadNaming", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test DataEncapsulationViolation", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test SeparationOfLogicAndInteraction", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test TooComplexCode", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test StaticMethods", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test StaticAttributeOfInstanceAttribute", List.of()),
                 Arguments.of("Test FinalModifier", List.of("MethodArgumentCouldBeFinal", "LocalVariableCouldBeFinal"))
-//                Arguments.of("Test ParsingIntegerValues", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test ToStringVsEquals", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test DoNotUseObject", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test ClassInsteadOfInterface", List.of()),
-//                Arguments.of("Test EnumForClosedSet", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test EmptyBlock", List.of()),
-//                Arguments.of("Test PackageUsage", List.of(RULE_PREFIX + )),
-//                Arguments.of("Test DynamicBinding", List.of(RULE_PREFIX + )),
         );
     }
 
