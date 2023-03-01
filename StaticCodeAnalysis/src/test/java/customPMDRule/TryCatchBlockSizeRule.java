d1448504dafed5659d92a2bd8836b4af7c5a814e
David Laubenstein <david.laubenstein@posteo.de>
2023-03-01 11:02:03 +0100

    Fix Try Catch block rule

--- StaticCodeAnalysis/src/test/java/customPMDRule/TryCatchBlockSizeRule.java
+++ StaticCodeAnalysis/src/test/java/customPMDRule/TryCatchBlockSizeRule.java
@@ -1,54 +1,22 @@
 package customPMDRule;
 
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTBlock;
-import net.sourceforge.pmd.lang.java.ast.ASTStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTTryStatement;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class TryCatchBlockSizeRule extends AbstractJavaRule {
-    private static final int MAX_LENGTH = 10;
-    private static final List<String> IRRELEVANT_STATEMENTS = Arrays.asList("System.out.println", "logger.info");
-
+    // NOTE: If you modify the {@value MAX_LENGTH} value,
+    // please make sure to also adjust the value in the custom-pmd-ruleset.xml file
+    // in the description of the rule
+    private static final int MAX_LENGTH = 20;
     @Override
     public Object visit(ASTTryStatement node, Object data) {
-        int length = node.jjtGetNumChildren();
+        // inside the ASTTryStatement node, there is a child node that contains
+        // all the content of the try block in the first child,
+        // so we have to count the number of elements inside the try block
+        int length = node.getChild(0).getNumChildren();
         if (length > MAX_LENGTH) {
-            int relevantStatements = 0;
-            for (int i = 0; i < length; i++) {
-                Node child = node.jjtGetChild(i);
-                if (!(child instanceof ASTBlock)) {
-                    relevantStatements++;
-                } else {
-                    for (int j = 0; j < child.jjtGetNumChildren(); j++) {
-                        Node grandchild = child.jjtGetChild(j);
-                        if (!(grandchild instanceof ASTStatement)) {
-                            continue;
-                        }
-                        ASTStatement statement = (ASTStatement) grandchild;
-                        if (isRelevant(statement)) {
-                            relevantStatements++;
-                        }
-                    }
-                }
-            }
-            if (relevantStatements <= MAX_LENGTH) {
-                addViolation(data, node);
-            }
+            addViolation(data, node);
         }
         return super.visit(node, data);
-    }
-
-    private boolean isRelevant(ASTStatement statement) {
-        String code = statement.toString();
-        for (String irrelevantStatement : IRRELEVANT_STATEMENTS) {
-            if (code.contains(irrelevantStatement)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
